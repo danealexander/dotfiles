50a0564591a95aa357e3d15c8a2bdf774d6c5431
Dane Groves <danealexander@users.noreply.github.com>
2019-05-15 18:52:31 -0400

    Simplified files to only use config.bash Defer initialization of nvm until nvm, node or a node-dependent command is run to speed up shell launching Updated node install

--- nodejs/completion.bash
+++
@@ -1,4 +0,0 @@
-# Set up Bash completion for node family
-
-# nvm
-[[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion
--- nodejs/path.bash
+++
@@ -1,7 +0,0 @@
-### nvm
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
-# set path for Yarn
-export PATH="$HOME/.yarn/bin:$PATH"
