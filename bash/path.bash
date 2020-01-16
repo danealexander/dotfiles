409618e0d20bf5c41d805ccc23c3e8f43dc376d6
Dane Groves <danealexander@users.noreply.github.com>
2020-01-15 20:26:40 -0500

    Added VS Code to path

--- bash/path.bash
+++ bash/path.bash
@@ -1,5 +1,9 @@
-export PATH="/usr/local/bin:/usr/local/sbin:${PATH}:${HOME}/bin:${HOME}/slisbin:${DOTS}/bin";
+export PATH="/usr/local/bin:/usr/local/sbin:${PATH}:${HOME}/bin:${HOME}/slisbin:${DOTS}/bin:";
 # Basic PATH
 #export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin";
+
+# Add Visual Studio Code (code)
+export PATH="${PATH}:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
 
 export MANPATH="/usr/local/share/man:$MANPATH";
