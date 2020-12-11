335b735538fef4d52935c1987636f56bd0b64931
danealexander <danealexander@users.noreply.github.com>
2020-12-11 13:44:21 -0500

    Added most used functions Marked functions loading as TODO

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,3 +1,12 @@
+# case insensitive path-completion
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
+
+
+
+
+
+
+
 # Load zsh completions
 if type brew &>/dev/null; then
   FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
