6b7315a1682fcef2937305bc6c0cb77be7478e4d
danealexander <danealexander@users.noreply.github.com>
2021-01-21 12:43:13 -0500

    Updated zsh path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,8 @@
 # Set path
-export PATH="/usr/local/sbin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:${PATH}:${HOME}/bin:${HOME}/slisbin:${DOTS}/bin:";
+
+# Add Visual Studio Code (code)
+export PATH="${PATH}:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+
+export MANPATH="/usr/local/share/man:$MANPATH";
